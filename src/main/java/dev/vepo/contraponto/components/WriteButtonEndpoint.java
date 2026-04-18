e42d0b9a5952eda124fc47111ebd6e926cf3c447
Victor Osório <victor.perticarrari@gmail.com>
2026-04-18 01:02:25 -0300

    Write Button

---
+++ src/main/java/dev/vepo/contraponto/components/WriteButtonEndpoint.java
@@ -0,0 +1,41 @@
+package dev.vepo.contraponto.components;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.vepo.contraponto.shared.infra.LoggedUser;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@ApplicationScoped
+@Path("components/write-btn")
+public class WriteButtonEndpoint {
+    private static final Logger logger = LoggerFactory.getLogger(MenuEndpoint.class);
+
+    @CheckedTemplate
+    public class Template {
+        public static native TemplateInstance writeBtn(LoggedUser user);
+    }
+
+    private final LoggedUser loggedUser;
+
+    @Inject
+    public WriteButtonEndpoint(LoggedUser loggedUser) {
+        this.loggedUser = loggedUser;
+    }
+
+    @GET
+    @Operation(hidden = true)
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance menu() {
+        logger.info("Reloading meny...");
+        return Template.writeBtn(loggedUser);
+    }
+}
