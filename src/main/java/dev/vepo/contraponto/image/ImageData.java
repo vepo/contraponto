2e834af517b6a07ad14f36f87e3cb7e31d4b30e5
Victor Osório <victor.perticarrari@gmail.com>
2026-05-07 15:39:47 -0300

    Fixing sonar issues

--- src/main/java/dev/vepo/contraponto/image/ImageData.java
+++ src/main/java/dev/vepo/contraponto/image/ImageData.java
@@ -1,3 +1,28 @@
 package dev.vepo.contraponto.image;
 
-public record ImageData(byte[] data, String contentType, long size) {}
+import java.util.Arrays;
+import java.util.Objects;
+
+public record ImageData(byte[] data, String contentType, long size) {
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        ImageData other = (ImageData) obj;
+        return size == other.size && Objects.equals(contentType, other.contentType) && Arrays.equals(data, other.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(size, contentType, Objects.hashCode(data));
+    }
+
+    @Override
+    public String toString() {
+        return "ImageData[contentType=%s, size=%d, data=%s]".formatted(contentType, size, Arrays.toString(data));
+    }
+}
